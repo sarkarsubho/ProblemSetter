b32e0acbc0e522be4a9c754fbacea3b47f3cbc0c
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2023-01-19 14:22:23 +0530

    second question completed with test cases

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 function isPrime(num) {
   if (num < 2) {
     return false;
@@ -14,12 +15,12 @@ function isPrime(num) {
 function checkPrime(arr) {
   let isArray = Array.isArray(arr);
   if (!isArray) {
-    return "Praramiter should be an array";
+    return "Parameter should be an array";
   }
 
-  for(let i=0;i<arr.length;i++){
-    if(isNaN(arr[i])){
-      return "arr Should only content integers but found nonIntegers"
+  for (let i = 0; i < arr.length; i++) {
+    if (isNaN(arr[i])) {
+      return "arr Should only content integers but found nonIntegers";
     }
   }
 
@@ -28,4 +29,102 @@ function checkPrime(arr) {
   return ans;
 }
 
-module.exports = { checkPrime };
+// Problem2
+
+let data = [
+  {
+    name: "Hewie",
+    age: 9,
+    promoted: true,
+    gender: "Male",
+  },
+  {
+    name: "Brantley",
+    age: 14,
+    promoted: true,
+    gender: "Female",
+  },
+  {
+    name: "Sophie",
+    age: 8,
+    promoted: true,
+    gender: "Female",
+  },
+  {
+    name: "alvin",
+    age: 14,
+    promoted: false,
+    gender: "Male",
+  },
+];
+
+/* expected out put
+{
+    "totalPromoted": 7,
+    "totalPromotedAvgAge": 16,
+    "totalPromotedUnder15": 6,
+    "totalAgePromotedMale": 52,
+    "totalPromotedFemaleAscByAge": [
+      { "name": "Dalenna", "age": 10, "promoted": true, "gender": "Female" },
+      { "name": "Reggis", "age": 12, "promoted": true, "gender": "Female" },
+      { "name": "Jacobo", "age": 13, "promoted": true, "gender": "Female" }
+    ]
+  }
+
+*/
+
+function checkPromoted(data) {
+  let isArray = Array.isArray(data);
+  if (!isArray) {
+    return "Input should be an array";
+  }
+
+  let totalPromoted = 0;
+  let totalPromotedUnder15 = 0;
+  let totalAgePromotedMale = 0;
+
+  let promotedAge = 0;
+  let totalPromotedFemale = [];
+
+  data.forEach((element) => {
+    if (element.promoted) {
+      totalPromoted++;
+      promotedAge += element.age;
+
+      if (element.age < 15) {
+        totalPromotedUnder15++;
+      }
+
+      if (element.gender === "Male") {
+        totalAgePromotedMale += element.age;
+      }
+
+      if (element.gender === "Female") {
+        totalPromotedFemale.push(element);
+      }
+    }
+  });
+
+  let totalPromotedAvgAge = (promotedAge / totalPromoted) | 0;
+
+  let totalPromotedFemaleAscByAge = totalPromotedFemale.sort(
+    (a, b) => a.age - b.age
+  );
+
+  return {
+    totalPromoted,
+    totalPromotedAvgAge,
+    totalPromotedUnder15,
+    totalAgePromotedMale,
+    totalPromotedFemaleAscByAge,
+  };
+}
+
+// Problem3
+
+
+let checkApiResponse = () => {
+}
+
+
+module.exports = { checkPrime, checkPromoted ,checkApiResponse };
--- index.test.js
+++ index.test.js
@@ -1,9 +1,72 @@
-const { checkPrime } = require("./index");
+const { checkPrime,checkPromoted ,checkApiResponse} = require("./index");
+
+const mockData=[
+  {
+    name: "Hewie",
+    age: 9,
+    promoted: true,
+    gender: "Male",
+  },
+  {
+    name: "Brantley",
+    age: 14,
+    promoted: true,
+    gender: "Female",
+  },
+  {
+    name: "Sophie",
+    age: 8,
+    promoted: true,
+    gender: "Female",
+  },
+  {
+    name: "alvin",
+    age: 14,
+    promoted: false,
+    gender: "Male",
+  },
+];
+
+const mockAns={
+  totalPromoted: 3,
+  totalPromotedAvgAge: 10,
+  totalPromotedUnder15: 3,
+  totalAgePromotedMale: 9,
+  totalPromotedFemaleAscByAge: [
+    { name: 'Sophie', age: 8, promoted: true, gender: 'Female' },
+    { name: 'Brantley', age: 14, promoted: true, gender: 'Female' }
+  ]
+};
+
+// api Data
+
+const mockApiResponse=[
+  {
+    "name": "house2",
+    "ownerName": "Krishna",
+    "address": "Patna",
+    "areaCode": "100000",
+    "rent": "1100",
+    "isBachelorAllowed": true,
+    "isMarriedAllowed": true,
+    "id": 2
+  },
+  {
+    "name": "house3",
+    "ownerName": "Bicky",
+    "address": "Bangal",
+    "areaCode": "300000",
+    "rent": "900",
+    "isBachelorAllowed": true,
+    "isMarriedAllowed": false,
+    "id": 3
+  }
+]
 
 describe("testing find Prime Function", () => {
 
   test("should return error message if the argument is not an array", () => {
-    expect(checkPrime(1)).toBe("Praramiter should be an array");
+    expect(checkPrime(1)).toBe("Parameter should be an array");
   });
 
   test("should return error message if the input have any nonIntegers", () => {
@@ -27,3 +90,51 @@ describe("testing find Prime Function", () => {
   });
 
 });
+
+describe("testing 2nd Function, promoted and not promoted  students.",()=>{
+  
+  test("should return error message if the argument is not an array", () => {
+    expect(checkPromoted({
+      name: "Sophie",
+      age: 8,
+      promoted: true,
+      gender: "Female",
+    })).toBe("Input should be an array");
+  });
+
+  test("should return the correct answer for totalPromoted", () => {
+    expect(checkPromoted(mockData).totalPromoted).toEqual(mockAns.totalPromoted);
+  });
+
+  test("should return the correct answer for totalPromotedAvgAge", () => {
+    expect(checkPromoted(mockData).totalPromotedAvgAge).toEqual(mockAns.totalPromotedAvgAge);
+  });
+
+  test("should return the correct answer for totalPromotedUnder15", () => {
+    expect(checkPromoted(mockData).totalPromotedUnder15).toEqual(mockAns.totalPromotedUnder15);
+  });
+
+
+  test("should return the correct answer for totalAgePromotedMale", () => {
+    expect(checkPromoted(mockData).totalAgePromotedMale).toEqual(mockAns.totalAgePromotedMale);
+  });
+
+  test("should return the correct answer for totalPromotedFemaleAscByAge ascending value", () => {
+    expect(checkPromoted(mockData).totalPromotedFemaleAscByAge[0]).toEqual(mockAns.totalPromotedFemaleAscByAge[0]);
+  });
+
+  test("should return the correct answer for totalPromotedFemaleAscByAge ascending value2", () => {
+    expect(checkPromoted(mockData).totalPromotedFemaleAscByAge[1]).toEqual(mockAns.totalPromotedFemaleAscByAge[1]);
+  });
+
+
+  test("should return the correct answer", () => {
+    expect(checkPromoted(mockData)).toEqual(mockAns);
+  });
+
+});
+
+describe("checking the api call is made and return the correct response",()=>{
+
+
+})
